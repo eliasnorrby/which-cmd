a4606e36727e228e15d08ffedb70402f77dde40a
Elias Norrby <elias.norrby@gmail.com>
2024-11-27 00:46:56 +0100

    feat: implement get command

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -1,4 +1,11 @@
+use std::fs;
+
+use crate::constants::*;
+
 pub fn get_command() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Getting command");
+    let xdg_dirs = xdg::BaseDirectories::with_prefix(PREFIX)?;
+    let output_path = xdg_dirs.place_data_file(OUTPUT_FILE_NAME)?;
+    let contents = fs::read_to_string(output_path)?;
+    println!("{}", contents);
     Ok(())
 }
