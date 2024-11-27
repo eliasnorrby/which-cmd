1c8239cb7f2937f477f3163b0d47dccb02e7ed30
Elias Norrby <elias.norrby@gmail.com>
2024-11-27 01:42:55 +0100

    fix: update shell integrations

--- src/commands/integration.rs
+++ src/commands/integration.rs
@@ -3,17 +3,48 @@ pub fn integration_command(shell: &str) -> Result<(), Box<dyn std::error::Error>
         "zsh" => {
             println!(
                 r#"
-# which-cmd Integration for zsh
+# which-cmd integration for zsh
 which_cmd_widget() {{
     local result
-    result=$(<$TTY which-cmd)
+    <$TTY which-cmd build
     if [[ $? -eq 0 ]]; then
+        result=$(which-cmd get)
         LBUFFER+="$result"
     fi
     zle reset-prompt
 }}
 zle -N which_cmd_widget
 bindkey '^P' which_cmd_widget
+"#
+            );
+        }
+        "zsh-tmux" => {
+            println!(
+                r#"
+# which-cmd integration for zsh + tmux
+which_cmd_tmux_widget() {{
+  if [[ $LBUFFER == "" ]]; then
+    local result
+    tmux display-popup -S fg=brightblack -T '#[fg=white bold] which-cmd #[fg=default]' -y S -w 95% -h 12 -b rounded -E "which-cmd build --immediate"
+    result=$(which-cmd get)
+    if [[ $result != "" ]]; then
+      if [[ $result = __IMMEDIATE__* ]]; then
+        local cmd
+        cmd=$(echo $result | cut -d' ' -f2-)
+        LBUFFER+="$cmd"
+        zle accept-line
+      else
+        LBUFFER+="$result"
+        zle self-insert
+      fi
+    fi
+    zle reset-prompt
+  else
+    zle self-insert
+  fi
+}}
+zle -N which_cmd_tmux_widget
+bindkey ' ' which_cmd_tmux_widget
 "#
             );
         }
