1f1d7b3c9b482cb671abd6af8794c08dfe42a0a7
Elias Norrby <elias.norrby@gmail.com>
2024-12-04 15:11:03 +0100

    remove outfile after reading

--- src/commands/get.rs
+++ src/commands/get.rs
@@ -5,7 +5,8 @@ use crate::constants::*;
 pub fn get_command() -> Result<(), Box<dyn std::error::Error>> {
     let xdg_dirs = xdg::BaseDirectories::with_prefix(PREFIX)?;
     let output_path = xdg_dirs.place_data_file(OUTPUT_FILE_NAME)?;
-    let contents = fs::read_to_string(output_path)?;
+    let contents = fs::read_to_string(&output_path)?;
+    fs::remove_file(&output_path)?;
     println!("{}", contents);
     Ok(())
 }
